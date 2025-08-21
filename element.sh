0379bfe0c083963fab1f530d73a182067ad3f052
Timi Rus <timi.rus92@gmail.com>
2025-08-21 12:57:51 -0400

    feat: adding identification of input

--- element.sh
+++ element.sh
@@ -2,4 +2,25 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-echo Please provide an element as an argument.
+GET_ELEMENT() {
+  # if no input
+  if [[ ! $1 ]]
+  then
+    echo Please provide an element as an argument.
+  # if input is atomic number
+  elif [[ $1 =~ ^[0-9]+$ ]]
+  then
+    echo Atomic number detected
+  # if input is symbol
+  elif [[ $1 =~ ^[A-Z]{1,2}$ ]]
+  then
+    echo Symbol detected
+  # if input is name
+  else
+    echo echo Name detected
+  # if input is not valid
+  fi
+}
+
+
+GET_ELEMENT $1
