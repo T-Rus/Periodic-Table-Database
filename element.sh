a0f8ee8be865d8dc17328443c91ca1584ed7028c
Timi Rus <timi.rus92@gmail.com>
2025-08-21 17:13:30 -0400

    feat: adding output function

--- element.sh
+++ element.sh
@@ -22,5 +22,21 @@ GET_ELEMENT() {
   fi
 }
 
+OUTPUT_INFO() {
+  if [[ ! $1 ]]
+  then
+    echo I could not find that element in the database.
+  else
+  # Find all information from atomic number as argument
+  ELEMENT=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements LEFT JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $1")
+  echo "$ELEMENT" | sed 's/|/ /g' | while read NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+  do
+    # Print output with info
+    echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
+  fi
+}
+
+OUTPUT_INFO $1
 
 GET_ELEMENT $1
