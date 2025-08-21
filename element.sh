6ad19592b6578568e82c1b34b85365f6ff58aba0
Timi Rus <timi.rus92@gmail.com>
2025-08-21 17:27:18 -0400

    feat: completing input processing

--- element.sh
+++ element.sh
@@ -10,56 +10,36 @@ GET_ELEMENT() {
   # if input is atomic number
   elif [[ $1 =~ ^[0-9]+$ ]]
   then
-    echo Atomic number detected
     # get number
     NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
-    echo $NUMBER
-    # if not found
-    if [[ -z $NUMBER ]]
-    then 
-      echo not found
-    fi
+    OUTPUT_INFO $NUMBER
   # if input is symbol
   elif [[ $1 =~ ^[A-Z]{1,2}$ ]]
   then
-    echo Symbol detected
     # get number
     NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1'")
-    echo $NUMBER
-    # if not found
-    if [[ -z $NUMBER ]]
-    then 
-      echo not found
-    fi
+    OUTPUT_INFO $NUMBER
   # if input is name or invalid input
   else
-    echo echo Name detected
     # get number
     NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$1'")
-    echo $NUMBER
-    # if not found
-    if [[ -z $NUMBER ]]
-    then 
-      echo not found
-    fi
+    OUTPUT_INFO $NUMBER
   fi
 }
 
 OUTPUT_INFO() {
-  if [[ ! $1 ]]
+  if [[ ! $NUMBER ]]
   then
     echo I could not find that element in the database.
   else
   # Find all information from atomic number as argument
-  ELEMENT=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements LEFT JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $1")
+  ELEMENT=$($PSQL "SELECT name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements LEFT JOIN properties ON elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $NUMBER")
   echo "$ELEMENT" | sed 's/|/ /g' | while read NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
   do
     # Print output with info
-    echo "The element with atomic number $1 is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    echo "The element with atomic number $NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   done
   fi
 }
-
-OUTPUT_INFO $1
 
 GET_ELEMENT $1
